9cf476332227d32e75abe6a17fa312e975508915
aLenz7891 <leiji@web.de>
2018-03-04 22:22:14 +0100

    Twitch-LiveStream-Resultate, Wechsel von ListView zu DataGridView, DesignAnpassung

--- NT Verwaltung/FormMain.cs
+++ NT Verwaltung/FormMain.cs
@@ -1,7 +1,7 @@
 ﻿using System;
-using System.Diagnostics;
 using System.Drawing;
 using System.IO;
+using System.Timers;
 using System.Windows.Forms;
 
 
@@ -9,22 +9,42 @@ namespace NT_Verwaltung
 {
 	public partial class FormMain : Form
 	{
-		public FormMain()
+        public FormMain()
 		{
 			InitializeComponent();
 		}
 
 		private void FormMain_Load(object sender, EventArgs e)
 		{
-			cbTable.SelectedIndexChanged += new EventHandler(Table_SIC);
-			SetPictures();
-			AddColumnHeader();
-			AddTables();
-			CurrentClock();
-			ReadOutSystemTXT();
+            WriteToLog("<---> Programmstart  <--->"); // Programmstart-Meldung
+            cbTable.SelectedIndexChanged += new EventHandler(Table_SIC); // Aktueller Tisch-Melder als optische Rückmeldung
+			SetPictures(); // Setzt die Bilder fest, spätere Umwandlung Richtung dynamische HTML-Ansicht mit Icon-Zeichnung als SVG
+            FillDataGridView(); // Befüllt die DataGridView-Ansicht als Excel-gleiche Tabelle
+			AddTables(); // Befüllt ComboBox und Tabelle mit Tischen
+			CurrentClock(); // Setzt die aktuelle jeweilige Uhrzeit fest, sowohl RechtsUnten als auch für dtpStartZeit (Stunde + Minuten)
+			ReadOutSystemTXT(); // Liest die System.txt aus, um eine Übersicht der Spiele zu haben, aus welcher ausgesucht werden kann
 		}
 
-		private void SetPictures()
+        private void FormMain_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            WriteToLog("<---> Programmende  <--->");
+        }
+
+        /// <summary>
+        /// Befüllt die dgvList direkt mit Spalten und setzt Weite fest
+        /// </summary>
+        private void FillDataGridView()
+        {
+            dgvList.Columns.Add("cTisch", "Tisch");
+            dgvList.Columns.Add("cSystem", "System");
+            dgvList.Columns.Add("cStart", "Startzeit");
+            dgvList.ColumnHeadersDefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter;
+            dgvList.RowsDefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter;
+            dgvList.Columns[0].Width = 80;
+            WriteToLog("DataGridView befüllt.");
+        }
+
+        private void SetPictures()
 		{
 			pbPlan.Image = Image.FromFile("./bilder/ntPlan.png");
 			pbLogo.Image = Image.FromFile("./bilder/ntLogo.png");
@@ -74,144 +94,140 @@ namespace NT_Verwaltung
 			cbSystem.Items.AddRange(inhalt);
 		}
 
-		private void PrepareBlock()
-		{
-			string[] inhalt = File.ReadAllLines(Application.StartupPath + "\\Systeme.txt");
-			lvList.Items[cbTable.SelectedIndex].SubItems[dtpStartTime.Value.Hour - 5].Text = cbSystem.Text;
-			WriteToLog("Runde auf Tisch " + cbTable.SelectedIndex + 1 + " - System: " + cbSystem.Text + " - StartZeit:" + dtpStartTime.Text);
-
-		}
-
 		private void CurrentClock()
 		{
-			Timer t = new Timer();
-			t.Interval = 1000;
-			t.Tick += new EventHandler(timer_Tick);
-			t.Start();
-		}
+			System.Timers.Timer tTime = new System.Timers.Timer();
+			tTime.Interval = 1000;
+            tTime.AutoReset = true;
+            tTime.Elapsed += t_Elapsed;
+            tTime.Enabled = true;
+            System.Timers.Timer tActualCBTime = new System.Timers.Timer();
+            tActualCBTime.Interval = 6000; // 600000 = 10 Minutes
+            tActualCBTime.AutoReset = true;
+            tActualCBTime.Elapsed += tActualCBTime_Elapsed;
+            tActualCBTime.Enabled = true;
 
-		private void timer_Tick(object sender, EventArgs e)
+        }
+
+        /// <summary>
+        /// Startet als Invoke aus Timer.Thread die ComboBox-Aktualisierung
+        /// </summary>
+        private void tActualCBTime_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            if (InvokeRequired)
+            {
+                this.Invoke(new Action(() => SetCBTimeMinutes()));
+            }
+        }
+
+        /// <summary>
+        /// Setzt die jeweilige aktuelle Uhrzeit der ComboBox dtpStartZeit alle 10 Minuten hoch
+        /// </summary>
+        private void SetCBTimeMinutes()
+        {
+            int iMinuteByTens = DateTime.Now.Minute;
+            if (iMinuteByTens % 10 == 0) {
+                dtpStartTime.Value = new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day, DateTime.Now.Hour, iMinuteByTens, 0);
+            }
+        }
+
+        private void t_Elapsed(object sender, EventArgs e)
 		{
-			lCurrentTime.Text = DateTime.Now.ToLongTimeString();
+            if (InvokeRequired)
+            {
+                this.Invoke(new Action(() => SetTime()));
+            }
 		}
 
-		private void AddColumnHeader()
-		{
-			ColumnHeader header1;
-			header1 = new ColumnHeader();
-			header1.Text = "Tisch";
-			header1.TextAlign = HorizontalAlignment.Center;
-			header1.Width = 50;
-			lvList.Columns.Add(header1);
+        /// <summary>
+        /// Setzt die jeweilige Uhrzeit fest, die RechtsUnten angezeigt wird
+        /// </summary>
+        private void SetTime()
+        {
+            lCurrentTime.Text = DateTime.Now.ToLongTimeString();
+        }
 
-			ColumnHeader[] liste = CreateTimeHeaders();
-			foreach (ColumnHeader entry in liste)
-			{
-				lvList.Columns.Add(entry);
-			}
-		}
-
-		private ColumnHeader[] CreateTimeHeaders()
-		{
-			ColumnHeader[] sammlung = new ColumnHeader[18];
-			try
-			{
-				for (int i = 6; i < 24; i++)
-				{
-					ColumnHead oSpalte = new ColumnHead(i);
-					sammlung[i - 6] = oSpalte.Head();
-				}
-			}
-			catch (Exception ex)
-			{
-				WriteToLog(ex.Message + " " + ex.StackTrace);
-			}
-			return sammlung;
-		}
-
-		private void AddTables()
+        /// <summary>
+        /// Erstellt alle notwendigen Tischnummern für die ComboBox und das DataGridView
+        /// </summary>
+        private void AddTables()
 		{
 			for (int i = 1; i < 41; i++)
 			{
 				string table = "Tisch " + i.ToString();
-				ListViewItem item = new ListViewItem();
-				item.Text = table;
-				for (int y = 0; y < 18; y++)
-				{
-					item.SubItems.Add("");
-				}
+                dgvList.Rows.Add(table, String.Empty, String.Empty);
 				cbTable.Items.Add(table);
-				lvList.Items.Add(item);
 			}
 		}
 
-		private void cmdRundenstart_Click(object sender, EventArgs e)
+		/// <summary>
+        /// Eintragen der jeweiligen Systeme mitsamt Uhrzeit zu einer bestimmten Uhrzeit
+        /// </summary>
+        private void cmdRundenstart_Click(object sender, EventArgs e)
 		{
-			int dauer = Convert.ToInt32(tbLength.Text) > 18 ? 18 : Convert.ToInt32(tbLength.Text);
-			try
-			{
-				if (lvList.Items[cbTable.SelectedIndex].SubItems[dtpStartTime.Value.Hour - 5].Text != "")
-				{
-					lvList.Items[cbTable.SelectedIndex].SubItems[dtpStartTime.Value.Hour - 5].Text = "";
-				}
+            try
+            {
+                int iCurrentTable = cbTable.SelectedIndex;
+                //if (dtpStartTime.Value !== DateTime.Now.TimeOfDay){ SCREAM! }
+                dgvList.Rows[iCurrentTable].Cells[1].Value = cbSystem.Text;
+                dgvList.Rows[iCurrentTable].Cells[2].Value = dtpStartTime.Text;
+                WriteToLog("Runde auf Tisch " + (Convert.ToInt32(cbTable.SelectedIndex) + 1) + " - StartZeit: " + dtpStartTime.Text + " - System: " + cbSystem.Text);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Fehler beim Eintragen der Runde." + Environment.NewLine + "Fehler im Log dokumentiert", "M136-Fehler");
+                WriteToLog(ex.Message + Environment.NewLine + ex.StackTrace);
 
-				lvList.Items[cbTable.SelectedIndex].SubItems[dtpStartTime.Value.Hour - 5].Text = "*" + cbSystem.Text; // Erster Tisch
+            }
+        }
 
-				if ((dtpStartTime.Value.Hour + dauer) > 23)
-				{
-					dauer = 24 - dtpStartTime.Value.Hour;			
-				}
-				for (int i = 1; i < dauer; i++) // Alle Tische nach dem 1sten!
-				{
-						lvList.Items[cbTable.SelectedIndex].SubItems[dtpStartTime.Value.Hour - 5 + i].Text = cbSystem.Text;
-				}
-				WriteToLog("Runde auf Tisch " + cbTable.SelectedIndex + 1 + " - System: " + cbSystem.Text + " - StartZeit:" + dtpStartTime.Text);
-			}
-			catch (Exception ex)
-			{
-				MessageBox.Show("Fehler beim Eintragen der Runde." + Environment.NewLine + "Fehler im Log dokumentiert", "M136-Fehler");
-				WriteToLog(ex.Message + Environment.NewLine + ex.StackTrace);
-			}
-		}
+        /// <summary>
+        /// Schmeißt eine gewählte Zeileneingabe raus und vermerkt es entsprechend im Log
+        /// </summary>
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int iCurrentTable = cbTable.SelectedIndex;
+                dgvList.Rows[iCurrentTable].Cells[1].Value = String.Empty;
+                dgvList.Rows[iCurrentTable].Cells[2].Value = String.Empty;
+                WriteToLog("Runde auf Tisch " + (Convert.ToInt32(cbTable.SelectedIndex) + 1) + " wurde abgebrochen und aus dem Rundenlog gestrichen ");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Fehler beim Austragen der Runde." + Environment.NewLine + "Fehler im Log dokumentiert", "M197-Fehler");
+                WriteToLog(ex.Message + Environment.NewLine + ex.StackTrace);
 
-		public void WriteToLog(string sText)
+            }
+
+        }
+
+        /// <summary>
+        /// Bastelt aus den Informationen des Tages eine Übersicht zum späteren leichteren Einsehen
+        /// </summary>
+        private void btnSummarize_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                WriteToLog("Diese Zeile absichtlich sinnfrei!"); // Platzhalter bis mir einfällt, wie ich das passend bastle...txtCountings? interner System-int?
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Fehler beim Zusammenfassen!" + Environment.NewLine + "Fehler im Log dokumentiert", "M216-Fehler");
+                WriteToLog(ex.Message + Environment.NewLine + ex.StackTrace);
+            }
+
+        }
+
+        public void WriteToLog(string sText)
 		{
 			FileStream fs = new FileStream(Application.StartupPath + "\\test.log", FileMode.Append);
 			StreamWriter sw = new StreamWriter(fs);
-			string time = DateTime.Now.TimeOfDay.ToString();
+			string time = DateTime.Now.ToShortTimeString();
 			string writing = time + " " + sText;
 			sw.WriteLine(writing);
 			sw.Close();
 		}
 
-		//private void lvList_MouseDoubleClick(object sender, MouseEventArgs e)
-		//{
-		//	Point mousePosition = lvList.PointToClient(MousePosition);
-		//	ListViewHitTestInfo hit = lvList.HitTest(mousePosition);
-		//	int columnindex = hit.Item.SubItems.IndexOf(hit.SubItem);
-		//	MessageBox.Show(columnindex.ToString());
-		//	// Use hit.Item
-		//	// Use hit.SubItem
-		//}
-	}
-
-	public partial struct ColumnHead
-	{
-		public int iNumber;
-
-		public ColumnHead(int dtUhrzeit)
-		{
-			this.iNumber = dtUhrzeit;
-		}
-
-		public ColumnHeader Head()
-		{
-			ColumnHeader o;
-			o = new ColumnHeader();
-			o.Text = iNumber.ToString() + "h";
-			o.TextAlign = HorizontalAlignment.Center;
-			o.Width = 44;
-			return o;
-		}
-	}
+    }
 }
